4a4e3c9eef4bdec100cd71ae126e4d7a845bfb2a
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-16 20:07:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 def display_board
   row = Array.new(3, "   ")
   dash = "-----------"
-  puts "#{row.join("|")}"
-  puts "#{dash}"
-  puts "#{row.join("|")}"
-  puts "#{dash}"
-  puts "#{row.join("|")}"
+  top_rows = 2.times do
+    puts "#{row.join("|")}"
+    puts "#{dash}"
+  end
+  "#{top_rows}"
 end
