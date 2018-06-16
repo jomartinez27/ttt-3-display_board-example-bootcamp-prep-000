ccba256b0b2ef7faa9bc6f458a437d16b7f439d6
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-16 20:00:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  row0 = Array.new(3, "   ")
+  row = Array.new(3, "   ")
   dash = "-----------"
   puts "#{cell.join("|")}"
   puts "#{dash}"
